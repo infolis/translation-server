60b95f3b630867b6118c680cfcef56cb7ac9482f
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2015-11-09 10:17:42 +0100

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-pref("translation-server.translatorsDirectory", "/Users/simon/Desktop/Development/FS/zotero/translators");
-pref("translation-server.httpServer.port", "1969");
+pref("translation-server.translatorsDirectory", "/home/kbaierer/build/zotero-translation-server-bootstrap/translators");
+pref("translation-server.httpServer.port", "1234");
 pref("translation-server.debug.log", true);
 pref("translation-server.debug.time", true);
 pref("translation-server.debug.level", 5);
